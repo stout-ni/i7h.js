66a202e310bd27166ffdeb7650dbc951b8dece77
Stout Ni <hz.shito@gmail.com>
2024-03-30 13:02:08 +0800

    feat: replace with arrow function

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 export default function i18n(s: string) {
-  const _f = (r: string): string => {
+  return s.replace(/\w+/g, (r: string): string => {
     if (r.length <= 2) {
       return r;
     }
     return `${r[0]}${r.length - 2}${r.slice(-1)}`;
-  };
-
-  return s.replace(/\w+/g, _f);
+  });
 }
